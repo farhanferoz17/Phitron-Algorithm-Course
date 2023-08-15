049fd8714c43b7cb54794ce62765cd566f0bcdb0
Farhan Feroz <38178160+farhanferoz17@users.noreply.github.com>
2023-08-15 19:41:00 +0600

    Create Quick Find Implementation.cpp

---
+++ Graph/Disjoint Set Union/Quick Find Implementation.cpp
@@ -0,0 +1,81 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Quick Find
+
+void init_code()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif // ONLINE_JUDGE
+}
+
+const int N = 1e5+7;
+int n,m;
+int root[N];
+
+void make_set(int x) // O(1)
+{
+	root[x] = x;
+	return;
+}
+
+int find_set(int x)  // O(1)
+{
+	return root[x];
+}
+
+void union_set(int x, int y)  // O(n)
+{
+	int rx = find_set(x);
+	int ry = find_set(y);
+
+	if(rx!=ry)
+	{
+		//rx is the ultimate leader
+		for(int i=1;i<=n;i++)
+		{
+			if(root[i] == ry)
+				root[i] = rx;
+		}
+	}
+}
+
+void print_roots()
+{
+	for(int i=1;i<=n;i++)
+    {
+    	cout << root[i] << " ";
+    }
+    cout << endl;
+}
+
+bool connected(int x, int y) // O(1)
+{
+	return find_set(x) == find_set(y);
+}
+
+int main()
+{
+    init_code();
+    
+    cin >> n >> m;
+
+    for(int i=1;i<=n;i++)
+    	make_set(i);
+    print_roots();
+    for(int i=0;i<m;i++)
+    {
+    	int a,b;
+    	cin >> a >> b;
+    	union_set(a,b);
+    	print_roots();
+    }
+
+    cout << connected(1,7) << endl;
+    cout << connected(5,3) << endl;
+
+ 	return 0;
+    
+}
