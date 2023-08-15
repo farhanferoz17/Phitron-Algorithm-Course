d63df56e5cd31ca66f7d1ed05a561c4c79e31939
Farhan Feroz <38178160+farhanferoz17@users.noreply.github.com>
2023-08-15 20:10:50 +0600

    Create Union by Rank Implementation.cpp

---
+++ Graph/Disjoint Set Union/Union by Rank Implementation.cpp
@@ -0,0 +1,90 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Union by Rank
+
+void init_code()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif // ONLINE_JUDGE
+}
+
+const int N = 1e5+7;
+int n,m;
+vector<int> ranks(N,1);
+int root[N];
+
+void make_set(int x) // O(1)
+{
+	root[x] = x;
+}
+
+int find_set(int x)  // O(log n)
+{
+	if(x == root[x])
+		return x;
+	return find_set(root[x]);
+}
+
+void union_set(int x, int y)  // O(log n)
+{
+	int rx = find_set(x);
+	int ry = find_set(y);
+
+	if(rx!=ry)
+	{
+		if(ranks[rx] > ranks[ry])
+		{
+			root[ry] = rx;
+		}
+		else if(ranks[ry] > ranks[rx])
+		{
+			root[rx] = ry;
+		}
+		else
+		{
+			root[ry] = rx;
+			ranks[rx]++;
+		}
+	}
+}
+
+void print_roots()
+{
+	for(int i=1;i<=n;i++)
+    {
+    	cout << root[i] << " ";
+    }
+    cout << endl;
+}
+
+bool connected(int x, int y) // O(log n)
+{
+	return find_set(x) == find_set(y);
+}
+
+int main()
+{
+    init_code();
+    
+    cin >> n >> m;
+
+    for(int i=1;i<=n;i++)
+    	make_set(i);
+    print_roots();
+    for(int i=0;i<m;i++)
+    {
+    	int a,b;
+    	cin >> a >> b;
+    	union_set(a,b);
+    	print_roots();
+    }
+
+    cout << connected(1,7) << endl;
+    cout << connected(5,3) << endl;
+
+ 	return 0;
+    
+}
